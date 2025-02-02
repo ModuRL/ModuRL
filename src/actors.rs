0d6f6e7c88b9f4136575c98dec5cbf76ba44d5c0
Ethan Almloff <ethanalmloff@gmail.com>
2025-02-01 22:08:00 -0600

    implement DQN

--- src/actors.rs
+++ src/actors.rs
@@ -1,5 +1,5 @@
 use candle_core::Tensor;
-use candle_nn::Optimizer;
+use candle_nn::{Optimizer, VarBuilder};
 use rand::Rng;
 
 use crate::{Gym, Space};
@@ -10,17 +10,20 @@ pub trait Actor {
 
 /// Q-learning actor
 /// The most basic actor, which just takes the action with the highest Q-value.
-pub struct DQNActor<I, O> {
+pub struct DQNActor {
     q_network: Box<dyn candle_core::Module>,
     epsilon: f32,
-    action_space: Box<dyn Space<O>>,
-    observation_space: Box<dyn Space<I>>,
+    action_space: Box<dyn Space>,
+    observation_space: Box<dyn Space>,
 }
 
-impl<I, O> DQNActor<I, O> {
-    pub fn new(q_network: Box<dyn candle_core::Module>, epsilon: f32, gym: &dyn Gym<I, O>) -> Self {
-        let action_space = gym.action_space();
-        let observation_space = gym.observation_space();
+impl DQNActor {
+    pub fn new(
+        q_network: Box<dyn candle_core::Module>,
+        epsilon: f32,
+        action_space: Box<dyn Space>,
+        observation_space: Box<dyn Space>,
+    ) -> Self {
         Self {
             q_network,
             epsilon,
@@ -33,6 +36,17 @@ impl<I, O> DQNActor<I, O> {
 impl Actor for DQNActor {
     fn act(&mut self, observation: &Tensor) -> Tensor {
         let q_values = self.q_network.forward(observation).unwrap();
-        todo!()
+        if rand::rng().random_range(0.0..1.0) < self.epsilon {
+            self.action_space.sample(observation.device())
+        } else {
+            let max_q_value = q_values.max(1).unwrap();
+            let action = q_values
+                .eq(&max_q_value)
+                .unwrap()
+                .to_dtype(candle_core::DType::I64)
+                .unwrap();
+            action
+        }
     }
 }
+
